b95893bc72eaec4210ffdd40ae3be4389462238c
dave-funds <davidochuba5@gmail.com>
2023-12-13 03:01:27 -0800

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import GithubProvider from './context/Context.jsx'
+import  GithubProvider from './context/context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
